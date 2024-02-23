b019c18bdf25c92cdf8115bfb1c0833e6ff8a4f7
BiYeon <isly7959@naver.com>
2024-02-23 20:35:50 +0900

    update toc and breadcrumbs

--- src/Components/Breadcrumbs.js
+++ src/Components/Breadcrumbs.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./Breadcrumbs.css";
+import { useLocation } from "react-router-dom";
+
 function Breadcrumbs() {
+  const location = useLocation();
+
+  // 경로를 "/"로 분할하여 배열로 변환
+  let pathArray = location.pathname.split("/").filter(Boolean);
+  if (pathArray[0] !== "home") {
+    pathArray.unshift("home"); // "home" 요소를 배열의 맨 앞에 추가합니다.
+  }
   return (
     <nav className="breadcrumbs-container">
-      <div className="breadcrumbs">Home</div>
+      <div className="breadcrumbs">
+        {/* 각 경로 요소에 대한 링크를 생성 */}
+        {pathArray.map((path, index) => (
+          <span key={index}>
+            <a href={`/${path}`}>{path}</a>
+            {/* 경로 요소 사이에 구분자 추가 */}
+            {index < pathArray.length - 1 && " / "}
+          </span>
+        ))}
+      </div>
     </nav>
   );
 }
--- src/Components/TOC.js
+++ src/Components/TOC.js
@@ -3,7 +3,18 @@ import "./TOC.css";
 function TOC() {
   return (
     <div className="toc-container">
-      <div className="toc">으아아앙</div>
+      <div className="toc">
+        <img className="author_avatar" src="img/profileimg.png" alt="profile img" />
+        <div className="author_content">
+          <div className="author_name">비연</div>
+          <div className="author_bio">화이팅</div>
+        </div>
+        <div className="author_social">지역이메일깃허브인스타</div>
+        <ol>
+          <li>Study</li>
+          <li>Tech</li>
+        </ol>
+      </div>
     </div>
   );
 }
