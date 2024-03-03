fddb753fd7797e07985d48b5c7394b460a703e87
BiYeon <isly7959@naver.com>
2024-03-03 17:05:40 +0900

    update contact, about.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,33 +1,40 @@
 import React from "react";
 import "./Contact.css"; // Contact 페이지의 CSS 파일을 임포트합니다.
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
+import { faSquareEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faSquareInstagram } from "@fortawesome/free-brands-svg-icons";
 const Contact = () => {
   return (
     <div className="contact-container">
       <h2>Contact Me</h2>
       <div className="contact-info">
         <p>
-          Email: <a href="mailto:isly7959@gmail.com">isly7959@gmail.com</a>
+          <FontAwesomeIcon className="social_ico" icon={faLocationDot} /> Location: 대한민국 서울
         </p>
         <p>
-          Instagram:{" "}
+          <FontAwesomeIcon className="social_ico" icon={faSquareEnvelope} /> Email:{" "}
+          <a href="mailto:isly7959@gmail.com">isly7959@gmail.com</a>
+        </p>
+        <p>
+          <FontAwesomeIcon className="social_ico" icon={faSquareInstagram} /> Instagram:{" "}
           <a href="https://www.instagram.com/byun.ej/" target="_blank" rel="noreferrer">
             @byun.ej
           </a>
         </p>
         <p>
-          Github:{" "}
+          <FontAwesomeIcon className="social_ico" icon={faSquareGithub} /> Github:{" "}
           <a href="https://github.com/ByuN0-0" target="_blank" rel="noreferrer">
             ByuN0-0
           </a>
         </p>
         <p>
-          Baekjoon:{" "}
+          /&lt;&gt; Baekjoon:{" "}
           <a href="https://www.acmicpc.net/user/islyee" target="_blank" rel="noreferrer">
             islyee
           </a>
         </p>
-        <p>Location: 대한민국 서울</p>
       </div>
     </div>
   );
